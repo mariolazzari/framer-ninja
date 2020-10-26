ecc7fe6b22ec3ed773ed2a796f94ce6e897aca1d
Mario Lazzari <mario.lazzari@gmail.com>
2020-10-26 19:01:35 +0100

    Done lesson 6

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = [
@@ -18,15 +19,28 @@ const Toppings = ({ addTopping, pizza }) => {
         {toppings.map(topping => {
           let spanClass = pizza.toppings.includes(topping) ? "active" : "";
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
+            <motion.li
+              key={topping}
+              whileHover={{ scale: 1.3, color: "#f8e112", originX: 0 }}
+              transition={{ type: "spring", stiffness: 300 }}
+              onClick={() => addTopping(topping)}
+            >
               <span className={spanClass}>{topping}</span>
-            </li>
+            </motion.li>
           );
         })}
       </ul>
 
       <Link to="/order">
-        <button>Order</button>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgb(255,255,255)",
+            boxShadow: "0px 0px 8px rgb(255,255,255)",
+          }}
+        >
+          Order
+        </motion.button>
       </Link>
     </div>
   );
